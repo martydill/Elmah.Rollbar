628717f95b3662e75a1c165608ec9e0d9a6d4645
Marty Dill <marty@code-ninja.org>
2015-06-27 15:50:04 -0700

    Initial commit

---
+++ RollbarErrorModule.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web;
+using RollbarSharp;
+
+namespace Elmah.Rollbar
+{
+    public sealed class RollbarErrorModule : HttpModuleBase
+    {
+        private HttpApplication _application;
+
+        protected override bool SupportDiscoverability
+        {
+            get { return true; }
+        }
+
+        protected override void OnInit(HttpApplication application)
+        {
+            if (application == null)
+                throw new ArgumentNullException("application");
+
+            _application = application;
+            _application.Error += OnError;
+            ErrorSignal.Get(_application).Raised += OnErrorRaised;
+        }
+
+        protected override void OnDispose()
+        {
+            base.OnDispose();
+            ErrorSignal.Get(_application).Raised -= OnErrorRaised;
+            _application.Error -= OnError;
+        }
+
+        private void OnError(object sender, EventArgs e)
+        {
+            SendToRollbar(_application.Server.GetLastError());
+        }
+
+        private void OnErrorRaised(object sender, ErrorSignalEventArgs args)  
+        {
+            SendToRollbar(args.Exception);
+        }
+
+        private void SendToRollbar(Exception error)
+        {
+            try
+            {
+                if (error == null)
+                    throw new ArgumentNullException("error");
+
+                (new RollbarClient()).SendException(error);
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError(e.ToString());
+            }
+        }
+    }
+}
